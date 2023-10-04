68abc3f708b6005d2e46877c93caaa748f1088ee
Yudai Fukushima <49578068+granddaifuku@users.noreply.github.com>
2023-10-04 15:06:00 +0000

    feat: implement primitive motecarlo

--- ch06/src/main.rs
+++ ch06/src/main.rs
@@ -1,22 +1,56 @@
 use rand::Rng;
-use std::sync::Mutex;
+use std::{cmp::Ordering, sync::Mutex};
 
 use once_cell::sync::Lazy;
 
-const H: usize = 3;
-const W: usize = 3;
-const END_TURN: usize = 4;
+use montecarlo::primitive_montecarlo_action;
+
+const H: usize = 5;
+const W: usize = 5;
+const END_TURN: usize = 20;
 #[allow(non_upper_case_globals)]
 const dstr: &[&str] = &["DOWN", "UP", "RIGHT", "LEFT"];
 
-static RNG: Lazy<Mutex<rand::rngs::StdRng>> =
-    Lazy::new(|| Mutex::new(rand::SeedableRng::seed_from_u64(0)));
+#[derive(Debug)]
+struct RandomGenerator {
+    rng: rand::rngs::StdRng,
+}
+
+impl RandomGenerator {
+    fn new() -> Self {
+        Self {
+            rng: rand::SeedableRng::seed_from_u64(0),
+        }
+    }
+
+    fn set_rng(&mut self, seed: u64) {
+        self.rng = rand::SeedableRng::seed_from_u64(seed);
+    }
+
+    fn gen_range(&mut self, limit: usize) -> usize {
+        self.rng.gen_range(0..limit)
+    }
+}
+
+static GENERATOR: Lazy<Mutex<RandomGenerator>> = Lazy::new(|| Mutex::new(RandomGenerator::new()));
+
+fn init_random_generator(seed: u64) {
+    GENERATOR.lock().unwrap().set_rng(seed);
+}
 
 fn get_random(limit: usize) -> usize {
-    RNG.lock().unwrap().gen_range(0..limit)
+    GENERATOR.lock().unwrap().gen_range(limit)
 }
 
 struct Ai(String, Box<dyn Fn(&SimultaneousMazeState) -> usize>);
+
+#[derive(Debug)]
+enum WinningStatus {
+    First,
+    Second,
+    Draw,
+    None,
+}
 
 #[derive(Debug, Clone, Copy)]
 struct Character {
@@ -42,7 +76,7 @@ impl Character {
 }
 
 #[derive(Debug, Clone)]
-struct SimultaneousMazeState {
+pub struct SimultaneousMazeState {
     points: Vec<Vec<usize>>,
     turn: usize,
     characters: Vec<Character>,
@@ -54,13 +88,12 @@ impl SimultaneousMazeState {
     #[allow(non_upper_case_globals)]
     const dy: [i32; 4] = [0, 0, 1, -1];
 
-    fn new(seed: u64) -> Self {
-        let mut rng: rand::rngs::StdRng = rand::SeedableRng::seed_from_u64(seed);
+    fn new() -> Self {
         let mut points = vec![vec![0; H]; W];
 
         for i in 0..H {
             for j in 0..W / 2 + 1 {
-                let point = rng.gen_range(0..10);
+                let point = get_random(10);
                 if i == H / 2 && (j == W / 2 - 1 || j == W / 2 + 1) {
                     continue;
                 }
@@ -115,6 +148,28 @@ impl SimultaneousMazeState {
         actions
     }
 
+    fn get_winning_status(&self) -> WinningStatus {
+        if self.is_done() {
+            let score_0 = self.characters[0].game_score;
+            let score_1 = self.characters[1].game_score;
+            match score_0.cmp(&score_1) {
+                Ordering::Greater => WinningStatus::First,
+                Ordering::Less => WinningStatus::Second,
+                Ordering::Equal => WinningStatus::Draw,
+            }
+        } else {
+            WinningStatus::None
+        }
+    }
+
+    fn get_first_player_score_for_winning_rate(&self) -> f32 {
+        match self.get_winning_status() {
+            WinningStatus::First => 1.0,
+            WinningStatus::Second => 0.0,
+            _ => 0.5,
+        }
+    }
+
     fn to_string(&self) {
         println!("turn: {}", self.turn);
         for player_id in 0..self.characters.len() {
@@ -153,14 +208,73 @@ impl SimultaneousMazeState {
 }
 
 #[allow(dead_code)]
+mod montecarlo {
+    use crate::get_random;
+
+    use super::{random_action, SimultaneousMazeState, WinningStatus};
+
+    // The view from the player0
+    fn playout(state: &mut SimultaneousMazeState) -> f32 {
+        match state.get_winning_status() {
+            WinningStatus::First => 1.0,
+            WinningStatus::Second => 0.0,
+            WinningStatus::Draw => 0.5,
+            WinningStatus::None => {
+                state.advance(random_action(state, 0), random_action(state, 1));
+                playout(state)
+            }
+        }
+    }
+
+    pub fn primitive_montecarlo_action(
+        state: &SimultaneousMazeState,
+        player_id: usize,
+        playout_number: usize,
+    ) -> usize {
+        let my_legal_actions = state.legal_actions(player_id);
+        let opp_legal_actions = state.legal_actions((player_id + 1) % 2);
+        let mut best_value = f32::MIN;
+        let mut best_action_index = -1;
+
+        for (i, act) in my_legal_actions.iter().enumerate() {
+            let mut value = 0.0;
+            for _ in 0..playout_number {
+                let mut next_state = state.clone();
+                if player_id == 0 {
+                    next_state
+                        .advance(*act, opp_legal_actions[get_random(opp_legal_actions.len())]);
+                } else {
+                    next_state
+                        .advance(opp_legal_actions[get_random(opp_legal_actions.len())], *act);
+                }
+                let player0_win_rate = playout(&mut next_state);
+                let win_rate = if player_id == 0 {
+                    player0_win_rate
+                } else {
+                    1.0 - player0_win_rate
+                };
+                value += win_rate;
+            }
+            if value > best_value {
+                best_value = value;
+                best_action_index = i as i32;
+            }
+        }
+
+        my_legal_actions[best_action_index as usize]
+    }
+}
+
+#[allow(dead_code)]
 fn random_action(state: &SimultaneousMazeState, player_id: usize) -> usize {
     let legal_actions = state.legal_actions(player_id);
 
     legal_actions[get_random(legal_actions.len())]
 }
 
-fn play_game(ais: Vec<Ai>, seed: u64) {
-    let mut state = SimultaneousMazeState::new(seed);
+#[allow(dead_code)]
+fn play_game(ais: Vec<Ai>) {
+    let mut state = SimultaneousMazeState::new();
     state.to_string();
 
     while !state.is_done() {
@@ -171,11 +285,41 @@ fn play_game(ais: Vec<Ai>, seed: u64) {
     }
 }
 
+#[allow(dead_code)]
+fn test_first_player_win_rate(ais: Vec<Ai>, game_number: usize) {
+    let mut first_player_win_rate = 0.0;
+    for i in 0..game_number {
+        init_random_generator(i as u64);
+
+        let mut state = SimultaneousMazeState::new();
+        let first_ai = &ais[0];
+        let second_ai = &ais[1];
+        loop {
+            state.advance(first_ai.1(&state), second_ai.1(&state));
+            if state.is_done() {
+                break;
+            }
+        }
+        let win_rate_point = state.get_first_player_score_for_winning_rate();
+        if win_rate_point >= 0.0 {
+            state.to_string()
+        }
+        first_player_win_rate += win_rate_point;
+
+        println!("i {} w {}", i, first_player_win_rate / (i + 1) as f32);
+    }
+    first_player_win_rate /= game_number as f32;
+    println!(
+        "Winning rate of {} to {} : {}",
+        &ais[0].0, &ais[1].0, first_player_win_rate
+    );
+}
+
 fn main() {
     let ais = vec![
         Ai(
-            String::from("randomAction"),
-            Box::new(|state| random_action(state, 0)),
+            String::from("primitiveMotecarloAction"),
+            Box::new(|state| primitive_montecarlo_action(state, 0, 1000)),
         ),
         Ai(
             String::from("randomAction"),
@@ -183,5 +327,5 @@ fn main() {
         ),
     ];
 
-    play_game(ais, 0);
+    test_first_player_win_rate(ais, 500);
 }
